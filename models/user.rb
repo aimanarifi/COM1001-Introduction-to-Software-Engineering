e48f67b470493a4f7e8cc59d5615a40cd1b6eee4
Alexander Johns <ajohns1@sheffield.ac.uk>
2022-02-22 18:02:09 +0000

    added database setup and post/user models

---
+++ models/user.rb
@@ -0,0 +1,23 @@
+# A user record from the database
+class User < Sequel::Model
+    def name
+        # Return full name of user
+        "#{first_name} #{last_name}"
+    end
+
+    def type_of_account
+        # Return name of account type
+        if account_type == 0
+            "Reporter"
+        elsif account_type == 1
+            "Viewer"
+        elsif account_type == 2
+            "Moderator"
+        elsif account_type == 3
+            "Administrator"
+        else
+            # If a wrong number is somehow entered, return nothing
+            ""
+        end
+    end
+end
