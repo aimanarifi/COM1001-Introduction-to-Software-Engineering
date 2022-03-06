15254b009dc9b57fe3e51082f41b51eea2733173
Alexander Johns <ajohns1@sheffield.ac.uk>
2022-03-06 22:47:21 +0000

    sorted post feed newest-first

--- controllers/feed.rb
+++ controllers/feed.rb
@@ -1,5 +1,9 @@
 get "/feed" do
     @posts = Post.all
 
+    # Posts added to DB in chronological order
+    # Reverse the list of posts to have newest-first
+    @posts = @posts.reverse
+
     erb :feed
 end
