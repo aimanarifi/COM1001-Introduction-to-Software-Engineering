dc9dd805bbe4e3266ce21f7ba965b94603703ee0
Alexander Johns <ajohns1@sheffield.ac.uk>
2022-03-07 15:08:26 +0000

    basic cookie checking to check if user is logged in

--- controllers/feed.rb
+++ controllers/feed.rb
@@ -1,9 +1,20 @@
-get "/feed" do
-    @posts = Post.all
+# Author: Alexander Johns
 
-    # Posts added to DB in chronological order
-    # Reverse the list of posts to have newest-first
-    @posts = @posts.reverse
+get "/" do
+    # Temporary, for testing:
+    response.set_cookie("is_logged_in", 1)
 
-    erb :feed
+    # If logged in, go to feed
+    # Else, go to log in 
+    if request.cookies["is_logged_in"].to_i == 1
+        @posts = Post.all
+
+        # Posts added to DB in chronological order
+        # Reverse the list of posts to have newest-first
+        @posts = @posts.reverse
+
+        erb :feed
+    else
+        erb :login
+    end
 end
--- controllers/new_post.rb
+++ controllers/new_post.rb
@@ -1,20 +1,28 @@
-# Create a new post
+# Author: Alexander Johns
 
 get "/new-post" do
-    @post = Post.new
-    erb :new_post
+    if request.cookies["is_logged_in"] == 1
+        @post = Post.new
+        erb :new_post
+    else
+        erb :login
+    end
 end
 
 post "/new-post" do
-    # Create new post, add parameters from form
-    @post = Post.new
-    @post.load(params)
+    if request.cookies["is_logged_in"] == 1
+        # Create new post, add parameters from form
+        @post = Post.new
+        @post.load(params)
 
-    # If object is valid, save changes to database, redirect to home
-    if @post.valid?
-        @post.save_changes
-        redirect "/feed"
+        # If valid post, save changes to db, redirect to feed
+        if @post.valid?
+            @post.save_changes
+            redirect "/"
+        end
+
+        erb :new_post
+    else
+        erb :login
     end
-
-    erb :new_post
 end
