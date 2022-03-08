2bc727e1c9acc9401f246cba9df01af50fd3c51f
Connor Wheadon <cdwheadon1@sheffield.ac.uk>
2022-03-08 00:09:08 +0000

    updating database structure

--- db/db.rb
+++ db/db.rb
@@ -22,3 +22,96 @@ unless defined?(DB)
     # Set up the Sequel databse instance
     DB = Sequel.sqlite(db, logger: Logger.new(log))
 end
+
+def create_tables
+    unless DB.table_exists?(:users)
+        DB.create_table :users do
+            primary_key :userID
+            column :username, String
+            column :password, String
+            column :email, String
+            column :first_name, String
+            column :last_name, String
+            column :is_suspended, Integer
+            column :account_type, Integer
+            foreign_key :universityID, :universities
+        end
+    end
+
+    unless DB.table_exists?(:posts)
+        DB.create_table :posts do
+            primary_key :postID
+            foreign_key :userID, :users
+            column :title, String
+            column :message, String
+            column :date_posted, String
+            column :is_moderated, Integer
+            column :is_image, Integer
+            column :image_link, String
+            foreign_key :universityID, :universities
+        end
+    end
+
+    #unless DB.table_exists?(:post_tags)
+        #DB.create_table :post_tags do
+            #primary_key :postID :tagID
+            #foreign_key :postID, :posts
+            #foreign_key :tagID, :tags
+        #end
+    #end
+
+    unless DB.table_exists?(:tags)
+        DB.create_table :tags do
+            primary_key :tagID
+            column :tag_name, String
+        end
+    end
+
+    unless DB.table_exists?(:universities)
+        DB.create_table :universities do
+            primary_key :universityID
+            column :university_Name, String
+        end
+    end
+
+    unless DB.table_exists?(:deleted_users)
+        DB.create_table :deleted_users do
+            primary_key :deleteID_user # this need tweaking on here and deleted posts
+            foreign_key :userID, :users
+            column date_deleted, String # this needs tweaking on here and deleted posts
+        end
+    end
+
+    unless DB.tables_exists?(:deleted_posts)
+        DB.create_table :deleted_posts do
+            primary_key :deleteID_post
+            foreign_key :userID, :users
+            column date_deleted, String
+        end
+    end
+
+    unless DB.tables_exists?(:reported_posts) # whole thing needs tweaking
+        DB.create_table :reported_posts do
+            primary_key :reported_postID #tweak this later
+            foreign_key :postID, :posts
+            foreign_key :report_reasonID, :report_reasons 
+            column :report_count, Integer
+        end
+    end
+
+    unless DB.tables_exists?(:report_reasons)
+        DB.create_tables :report_reasons do
+            primary_key :report_reasonID
+            column :report_name, String
+        end
+    end
+
+end
+
+
+create_tables
+
+
+
+
+
