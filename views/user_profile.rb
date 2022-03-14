e48848dd037c99aae10cef4d889b2f18fdc37c6f
Onaghalino Ekere <oaekere1@sheffield.ac.uk>
2022-03-14 12:08:11 +0000

    created the user profile

---
+++ views/user_profile.rb
@@ -0,0 +1,17 @@
+<%#load common header here%>
+
+<body>
+
+    <h1>MY PROFILE</h1>
+    <h1>Hello <%= "#{@user.first_name} {@user.last_name}"%></h1>
+
+     /* shows all the posts the user has posted */
+     <%if @posts.count > 0%>
+        <%@posts.each do |post|%>
+            <p><%= post.title%></p>
+            <p><%=post.message%></p>
+            <%else%>
+            <p>Make your first post</p>
+    <%end%>
+
+    <%#load common footer here%>
