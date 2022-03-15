4e10f33d8d6357b66d0dbf4874ecc3ffedf7ed53
Muhammad Kamaludin <maakamaludin1@sheffield.ac.uk>
2022-03-15 21:47:22 +0000

    Add user/account moderation

---
+++ controllers/account_moderation_feed.rb
@@ -0,0 +1,38 @@
+#Author: Muhammad Kamaludin
+get "/moderation-feed/users" do 
+
+    user_id = session[:userID]
+    redirect "/login" unless session[:logged_in]
+
+    unless params["filter"].nil?
+        #filter params with have the string "<filtertype>,<post/tagID>,<tag/uniname>"
+        @filter = params["filter"].split(",")
+        @filter_type = @filter[0]
+        @filter_id = @filter[1]
+        @filter_name = @filter[2]
+    else
+        @filter = nil
+    end
+
+    @user = User[user_id]  
+
+    unless @user.nil?
+        #Display all unmoderated post to admin, while for moderator, display posts th
+        if @user[:account_type] == 3
+
+            @user_type_query = "Moderator"
+            @account = User.where(account_type: 1).reverse
+            @account = @account.where(universityID: @filter) if !@filter.nil? && @filter_type == "uni"
+        
+        else
+            redirect "/"    
+        end
+
+    else
+        redirect "/"
+    end
+
+
+
+    erb :account_moderation_feed
+end
