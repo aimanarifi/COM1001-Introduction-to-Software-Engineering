2b5a841d77c7f9261a47d50f2cd724893bd2d9a7
Alexander Johns <ajohns1@sheffield.ac.uk>
2022-03-15 16:40:11 +0000

    user profile updates

--- controllers/user_profile.rb
+++ controllers/user_profile.rb
@@ -1,8 +1,8 @@
 get "/profile" do
     redirect "/login" unless session[:logged_in] == 1
 
-    @user = DB[:users].where(userID: session[:userID]).first
-    @posts = DB[:posts].where(userID: session[:userID])
+    @user = User.where(userID: session[:userID]).first
+    @posts = Post.where(userID: session[:userID])
 
     erb :user_profile
 end
