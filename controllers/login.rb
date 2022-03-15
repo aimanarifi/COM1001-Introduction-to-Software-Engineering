caaf1a5eefa2d41433a9fbb1637a2e1173e2a378
Alexander Johns <ajohns1@sheffield.ac.uk>
2022-03-15 09:44:15 +0000

    more session cookies

--- controllers/login.rb
+++ controllers/login.rb
@@ -13,21 +13,15 @@ post "/login" do
   @user.login(params)
   @error = nil
 
-  if @user.exist?
+  if @user.exist? && @user.valid?
     session[:logged_in] = true
+    session[:is_guest] = 0
+    session[:account_type] = @user.account_type
+    session[:userID] = @user.userID
+    session[:username] = @user.username
+    session[:universityID] = @user.universityID
+
     redirect "/"
-  else
-    @error = "The username or password that you entered is incorrect, please try again"
-  end
-
-
-  if @user.valid?
-    if @user.exist?
-      session[:logged_in] = true
-      redirect "/"
-    else
-      @error = "The username or password that you entered is incorrect, please try again"
-    end
   else
     @error = "The username or password that you entered is incorrect, please try again"
   end
