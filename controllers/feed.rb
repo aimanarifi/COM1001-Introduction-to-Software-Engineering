e9bd7bd178128f11b434820ea8bfc01d327b24ee
Alexander Johns <ajohns1@sheffield.ac.uk>
2022-03-16 18:25:06 +0000

    dont show feed if user is a reporter

--- controllers/feed.rb
+++ controllers/feed.rb
@@ -1,7 +1,7 @@
 get "/" do
     redirect "/login" unless session[:logged_in] == 1
 
-    redirect "/new-post" if session[:is_guest] == 1
+    redirect "/new-post" if session[:is_guest] == 1 || session[:account_type] == 0
 
     @posts = Post.where(is_moderated: 1).reverse
 
