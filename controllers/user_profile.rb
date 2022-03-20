06ce963004dfd984900fd754d2981986225aafbf
Alexander Johns <ajohns1@sheffield.ac.uk>
2022-03-20 20:28:40 +0000

    cant access profile if logged in as guest

--- controllers/user_profile.rb
+++ controllers/user_profile.rb
@@ -2,6 +2,7 @@
 
 get "/profile" do
     redirect "/login" unless session[:logged_in] == 1
+    redirect "/" if session[:is_guest] == 1
 
     @user = User[session[:userID]]
     @posts = Post.where(userID: session[:userID]).order(:date_posted).reverse
