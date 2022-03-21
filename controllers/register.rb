2c240a2fcf2bdf6a563eca7643a3432ac187b553
Raymon Narwal <rjsnarwal1@sheffield.ac.uk>
2022-03-21 08:53:51 +0000

    changes to register.rb and register.erb more bug fixing for registration page

--- controllers/register.rb
+++ controllers/register.rb
@@ -6,13 +6,12 @@ get "/register" do
     @user = User.new[id]
 
     erb :register
-    "register page"
 end
 
 post "/register" do
     redirect "/" if session[:logged_in] == 1
 
-    @user = User.new(userID: params["userID"], username: params["username"], password: params["password"], email: params["email"], first_name: params["first_name"])
+    @user = User.new(username: params["username"], password: params["password"], email: params["email"], first_name: params["first_name"])
     @user.save
     
     if @user.valid?
