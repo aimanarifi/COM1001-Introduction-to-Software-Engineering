fb17c3e48a876ea426086332f9a375f80b3564e5
Muhammad Kamaludin <maakamaludin1@sheffield.ac.uk>
2022-03-29 10:14:22 +0000

    added code for deleted user login

--- controllers/login.rb
+++ controllers/login.rb
@@ -3,7 +3,6 @@
 
 get "/login" do
   redirect "/" if session[:logged_in] == 1
-
   @user = User.new
   erb :login
 end
@@ -16,18 +15,24 @@ post "/login" do
   @error = nil
 
   if @user.exist? && @user.valid?
+    
     @logged_in_user = User.where(username: params.fetch("username","")).first || 
     @logged_in_user = User.where(email: params.fetch("username","")).first
-
     
-    session[:logged_in] = 1
-    session[:is_guest] = 0
-    session[:account_type] = @logged_in_user.account_type
-    session[:userID] = @logged_in_user.userID
-    session[:username] = @logged_in_user.username
-    session[:universityID] = @logged_in_user.universityID
+    if @logged_in_user.is_suspended != 1
+      session[:logged_in] = 1
+      session[:is_guest] = 0
+      session[:account_type] = @logged_in_user.account_type
+      session[:userID] = @logged_in_user.userID
+      session[:username] = @logged_in_user.username
+      session[:universityID] = @logged_in_user.universityID
 
-    redirect "/"
+      redirect "/"
+
+    else
+      @error = "The account has been suspended"
+    end
+
   else
     @error = "The username or password that you entered is incorrect, please try again"
   end
