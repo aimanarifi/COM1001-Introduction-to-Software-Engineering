3fcb5478852082252513fddbcd418afa489de0fd
Alexander Johns <ajohns1@sheffield.ac.uk>
2022-04-20 17:07:40 +0000

    spec changes

--- spec/features/feed_spec.rb
+++ spec/features/feed_spec.rb
@@ -3,11 +3,25 @@
 require_relative "../spec_helper"
 
 describe "the feed page" do
-    before(:all) { log_in_viewer }
-
     it "is accessible from the new post page" do
+        log_in_viewer
         visit "/new-post"
         click_link "Cancel"
         expect(page).to have_content "Feed"
+    end
+
+    it "will be empty when no posts exist" do
+        log_in_viewer
+        visit "/"
+        expect(page).to have_content "No posts were found."
+    end
+
+    it "will display new posts" do
+        log_in_admin
+        visit "/new-post"
+        add_test_post
+
+        expect(page).to have_content "By admin"
+        clear_database
     end
 end
--- spec/features/user_profile_spec.rb
+++ spec/features/user_profile_spec.rb
@@ -2,7 +2,7 @@
 
 require_relative "../spec_helper"
 
-describe "the new post page" do
+describe "the user profile page" do
     context "when accessing the page," do
         it "is not accessible when logged out" do
             visit "/logout"
@@ -16,5 +16,32 @@ describe "the new post page" do
             click_link "Profile"
             expect(page).to have_content "Profile"
         end
+    end
+
+    it "will display the user's details" do
+        log_in_viewer
+        visit "/profile"
+        expect(page).to have_content "Name: "
+        expect(page).to have_content "Email: "
+        expect(page).to have_content "Account Type: "
+        expect(page).to have_content "University: "
+    end
+
+    it "will be empty when user hasn't created any posts" do
+        log_in_viewer
+        visit "/profile"
+        expect(page).to have_content "No posts were found."
+    end
+
+    it "will display posts by this user" do
+        log_in_viewer
+        visit "/new-post"
+        add_test_post
+        Post.last.update(is_moderated: 1)
+
+        visit "/profile"
+        expect(page).to have_content "Tags:"
+
+        clear_database
     end
 end
