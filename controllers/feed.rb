dfdcdc8fb9ef262539665e2e44c7751e59f95807
Muhammad Kamaludin <maakamaludin1@sheffield.ac.uk>
2022-04-24 15:18:15 +0000

    Add filter to the feed

--- controllers/feed.rb
+++ controllers/feed.rb
@@ -5,6 +5,16 @@ get "/" do
 
     redirect "/new-post" if session[:is_guest] == 1 || session[:account_type] == 0
 
+    if params["filter"].nil? || params["filter"] == ""
+        @filter = nil
+    else
+        #filter params with have the string "<filtertype>,<post/tagID>,<tag/uniname>"
+        @filter = params["filter"].split(",")
+        @filter_type = @filter[0]
+        @filter_id = @filter[1]
+        @filter_name = @filter[2]    
+    end
+
     @posts = Post.where(is_moderated: 1).order(:date_posted).reverse
 
     erb :feed
