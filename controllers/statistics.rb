3b2529990318d618f180ffb88951f4931396679a
Muhammad Kamaludin <maakamaludin1@sheffield.ac.uk>
2022-04-24 15:16:48 +0000

    Add page for posts stats

---
+++ controllers/statistics.rb
@@ -0,0 +1,9 @@
+get "/stats" do
+    redirect "/login" unless session[:logged_in] == 1
+
+    redirect "/new-post" if session[:is_guest] == 1 || session[:account_type] == 0
+    
+    @posts = Post.where(is_moderated: 1)
+
+    erb :statistics
+end
